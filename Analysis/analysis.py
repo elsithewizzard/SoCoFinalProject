31b55defb17662a200038da95966b1dd5a191ad5
Z3lik0 <albruell@gmx.ch>
2026-05-19 19:21:47 +0200

    ready for analysis

--- Analysis/analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-basepath = os.getcwd()
-file_path = os.path.join(basepath, "Analysis", "data", "games_clean_summary.csv")
-
-df = pd.read_csv(file_path)
-
-print(f"Full Dataset: Total games: {len(df)}")
