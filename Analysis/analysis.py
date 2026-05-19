5723a996fbcb3bfbb159a15229d3930cbf82e64d
Z3lik0 <albruell@gmx.ch>
2026-05-19 19:20:22 +0200

    clean dataset ready for analysis (i believe)

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import os
 
-repo_root = os.getcwd()
-file_path = os.path.join(repo_root, "Data Collection", "MASTER_ALL_DATA.csv")
+basepath = os.getcwd()
+file_path = os.path.join(basepath, "Analysis", "data", "games_clean_summary.csv")
 
 df = pd.read_csv(file_path)
 
 print(f"Full Dataset: Total games: {len(df)}")
-df.head()
